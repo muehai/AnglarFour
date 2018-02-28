642316d8fa9bb9703904112bfcfae889a51dbf38
mh906276 <mh906276@SESTKN-L1408005>
2018-02-28 13:04:41 +0100

    dfa

--- Angular3Web/ClientApp/app/components/app.shared.module.ts
+++ Angular3Web/ClientApp/app/components/app.shared.module.ts
@@ -1,1 +1,52 @@
-﻿
+﻿import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Headers, RequestOptions, BaseRequestOptions } from '@angular/http';
+import { APP_BASE_HREF, CommonModule, Location, LocationStrategy, HashLocationStrategy } from '@angular/common';
+// third party module to display toast 
+import { ToastrModule } from 'toastr-ng2';
+//PRIMENG - Third party module
+import { InputTextModule, DataTableModule, ButtonModule, DialogModule } from 'primeng/primeng';
+
+import { AppComponent } from '../components/app/app.component';
+import { NavMenuComponent } from '../components/navmenu/navmenu.component';
+import { ContactComponent } from '../components/contact/contact.component';
+
+import { ContactService } from '../_service/index';
+
+class AppBaseRequestOptions extends BaseRequestOptions {
+    headers: Headers = new Headers();
+    constructor() {
+        super();
+        this.headers.append('Content-Type', 'application/json');
+        this.body = '';
+    }
+}
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        ContactComponent
+    ],
+    providers: [ContactService,
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        { provide: RequestOptions, useClass: AppBaseRequestOptions }],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
+        InputTextModule, DataTableModule, ButtonModule, DialogModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'contact', pathMatch: 'full' },
+            { path: 'contact', component: ContactComponent },
+            { path: '**', redirectTo: 'contact' }
+        ])
+    ]
+})
+export class AppModuleShared {
+}
